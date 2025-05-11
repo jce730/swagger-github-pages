5eb58d399d5a41273ed55fdddd241b271f53e4c6
che730 <che730@che730ui-MacBookPro.local>
2025-05-11 22:40:24 +0900

    openapi.yaml 에 server 추가 및 cors 설정 추가

---
+++ src/main/kotlin/com/bori/swaggergithubpages/WebConfig.kt
@@ -0,0 +1,14 @@
+package com.bori.swaggergithubpages
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://jce730.github.io")
+            .allowedMethods("*")
+    }
+}
