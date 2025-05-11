8de018b3e9947e86ec7762b70da79243ea2b8204
che730 <che730@che730ui-MacBookPro.local>
2025-05-11 22:12:49 +0900

    init

---
+++ src/main/kotlin/com/bori/swaggergithubpages/SwaggerGithubPagesApplication.kt
@@ -0,0 +1,11 @@
+package com.bori.swaggergithubpages
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwaggerGithubPagesApplication
+
+fun main(args: Array<String>) {
+    runApplication<SwaggerGithubPagesApplication>(*args)
+}
---
+++ src/test/kotlin/com/bori/swaggergithubpages/SwaggerGithubPagesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bori.swaggergithubpages
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwaggerGithubPagesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
